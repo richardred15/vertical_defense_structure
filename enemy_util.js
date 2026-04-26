f217932a576403b794c736628ca9c153c0c69e00
Richard R <richardred15@gmail.com>
2026-04-26 17:20:18 -0600

    Fixed issue with enemy spawning while window is not visible

--- enemy_util.js
+++ enemy_util.js
@@ -1,24 +1,22 @@
-function next_enemy() {
-    spawn_enemy();
-    setTimeout(next_enemy, 3000 - frameCount / 10);
-}
-
-setTimeout(next_enemy, 3000);
+let last_enemy_spawn_time = 0;
 function spawn_enemy() {
-    let spawn_location = new Vec2(0, 0);
-    if (Math.random() < 0.5) {
-        if (Math.random() < 0.5)
-            spawn_location = new Vec2(0, Math.random() * height);
-        else spawn_location = new Vec2(width, Math.random() * height);
-    } else {
-        if (Math.random() < 0.5)
-            spawn_location = new Vec2(Math.random() * width, 0);
-        else spawn_location = new Vec2(Math.random() * width, height);
+    if (Date.now() - last_enemy_spawn_time > 3000) {
+        last_enemy_spawn_time = Date.now();
+        let spawn_location = new Vec2(0, 0);
+        if (Math.random() < 0.5) {
+            if (Math.random() < 0.5)
+                spawn_location = new Vec2(0, Math.random() * height);
+            else spawn_location = new Vec2(width, Math.random() * height);
+        } else {
+            if (Math.random() < 0.5)
+                spawn_location = new Vec2(Math.random() * width, 0);
+            else spawn_location = new Vec2(Math.random() * width, height);
+        }
+        let enemy = new Enemy(spawn_location);
+        enemy.max_health = 50 + frameCount / 1000;
+        enemy.movement_speed = 1 + frameCount / 10000;
+        enemy.attack_damage = 1;
+        enemy.attacks("vds");
+        world.add_entity(enemy);
     }
-    let enemy = new Enemy(spawn_location);
-    enemy.max_health = 50 + frameCount / 1000;
-    enemy.movement_speed = 1 + frameCount / 10000;
-    enemy.attack_damage = 1;
-    enemy.attacks("vds");
-    world.add_entity(enemy);
 }
--- script.js
+++ script.js
@@ -20,7 +20,6 @@ function setup() {
     hud = new HUD(new Vec2(), new Vec2(width, height));
     initialize_hud();
     star_field = new StarField();
-    spawn_enemy();
     vds = new VDS(new Vec2(width / 2, height / 2));
     world.add_entity(vds);
 }
@@ -28,6 +27,8 @@ function setup() {
 function draw() {
     resetMatrix();
     background(5, 5, 20);
+    spawn_enemy();
+
     grid.draw();
     star_field.draw();
     world.update();
@@ -40,3 +41,13 @@ document.addEventListener("mouseup", (event) => {
     hud.mouse_clicked(new Vec2(mouseX, mouseY), event);
 });
 document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+document.addEventListener("visibilitychange", () => {
+    if (document.hidden) {
+        noLoop();
+        console.log("PAUSED");
+    } else {
+        loop();
+        console.log("RESUMED");
+    }
+});
